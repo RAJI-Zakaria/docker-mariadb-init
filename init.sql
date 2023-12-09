7b9811ed500310be8cbe17e65b8a9e86524050db
zak <zak@zaks-MacBook-Pro.local>
2023-12-09 17:49:27 +0100

    setting up docker and adding sql

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS db_dev;
 USE db_dev;
 
 -- Create user table
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS todo (
     id INT AUTO_INCREMENT PRIMARY KEY,
     task VARCHAR(255) NOT NULL,
     description TEXT,
@@ -12,8 +12,8 @@ CREATE TABLE IF NOT EXISTS user (
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
--- Create todo table
-CREATE TABLE IF NOT EXISTS todo (
+-- Create todo table 
+CREATE TABLE IF NOT EXISTS user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
